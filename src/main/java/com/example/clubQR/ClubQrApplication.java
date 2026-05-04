8d1042dbb52e8b86396d3cc36b787d598c4cc4ba
Andrew <vip.andrusha2000@mail.ru>
2026-05-04 17:50:41 +0500

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/clubQR/ClubQrApplication.java
@@ -0,0 +1,13 @@
+package com.example.clubQR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubQrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clubQR/ClubQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clubQR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
